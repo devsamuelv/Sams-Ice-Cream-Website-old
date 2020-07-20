e399b06961c8033e15cb49f3b682bb330d6bdca9
DevSamuelV <realalloutmusic@gmail.com>
2020-07-20 00:39:32 -0400

    removed api keys

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { ADMIN_KEY, READ_KEY } from '../../env';
+
+export const environment = {
+    production: true,
+    ADMIN_KEY: ADMIN_KEY,
+    READ_KEY: READ_KEY
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { ADMIN_KEY, READ_KEY } from '../../env';
+
+export const environment = {
+    production: false,
+    ADMIN_KEY: ADMIN_KEY,
+    READ_KEY: READ_KEY
+}
